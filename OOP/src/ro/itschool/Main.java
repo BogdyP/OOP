75a9a26b29d7c27ceafc76fb7d1d7981d4a5049b
Bogdan <bogdanpandalau@gmail.com>
2022-02-22 20:24:00 +0200

    house project

--- OOP/src/ro/itschool/Main.java
+++ OOP/src/ro/itschool/Main.java
@@ -3,6 +3,18 @@ package ro.itschool;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        House casaMea = new House("Gabi's House");
+        casaMea.color = "Verde";
+        casaMea.numberOfDoors = 10;
+
+        System.out.println("Metri patrati" + casaMea.meters);
+
+        House casaVecinului = new House("Gheorhe's House");
+        casaVecinului.color = "Albastru";
+        casaVecinului.numberOfDoors = 12;
+
+        User gabriel = new User("Ionescu","Gabriel" , 28);
+
     }
 }
